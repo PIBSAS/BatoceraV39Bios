c1f613fcc9c66dd723e3031162cc81c0c63de14a
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2023-12-21 14:59:53 -0300

    Update and rename pi5-bios.sh to pi5.sh

--- pi5.sh
+++ pi5.sh
@@ -14,7 +14,7 @@ echo
 echo "Limpiando las bios basura que te descargaste de otro lado o las mismas si se te corto internet"
 echo "Cleaning the garbage bios downloaded from other sites or from this site but if you internet shutdown in the process"
 echo
-RUTA=https://raw.githubusercontent.com/Luciano2018/BatoceraV39Bios/main/bios/
+RUTA=https://raw.githubusercontent.com/PIBSAS/BatoceraV39Bios/main/bios/
 RUTA_ARCH=https://archive.org/download/raspberry-pi-buenos-aires-batocera-v31-BIOS/Raspberry%20Pi%20Buenos%20Aires%20Batocera%20V35%20BIOS%20Extra%20PC.zip/bios/
 RUTA_TRI=https://archive.org/download/raspberry-pi-buenos-aires-batocera-v31-BIOS/Raspberry%20Pi%20Buenos%20Aires%20Batocera%20V33%20BIOS.zip/bios/
 RUTA_CUA=https://archive.org/download/raspberry-pi-buenos-aires-batocera-v31-BIOS/
