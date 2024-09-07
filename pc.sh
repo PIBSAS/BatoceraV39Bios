5debc7ce5c6220b280a38f2276383eb333906c23
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-09-07 19:06:40 -0300

    Update pc.sh

--- pc.sh
+++ pc.sh
@@ -14,7 +14,7 @@ echo
 echo "Limpiando las bios basura que te descargaste de otro lado o las mismas si se te corto internet"
 echo "Cleaning the garbage bios downloaded from other sites or from this site but if you internet shutdown in the process"
 echo
-RUTA=https://raw.githubusercontent.com/PIBSAS/BatoceraV39Bios/main/bios/
+RUTA="https://archive.org/download/batov39/"
 #RUTA_LFS=https://media.githubusercontent.com/media/PIBSAS/BatoceraV39Bios/main/bios/
 RUTA_ARCH="https://archive.org/download/bios_batocera/BatoceraV39Bios/bios/"
 #RUTA_TRI=https://archive.org/download/raspberry-pi-buenos-aires-batocera-v31-BIOS/Raspberry%20Pi%20Buenos%20Aires%20Batocera%20V33%20BIOS.zip/bios/
